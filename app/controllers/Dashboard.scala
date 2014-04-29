9a39328c53df2074f0557f3bab0ba6d997adc811
Julien DERAY <julien@deray.fr>
2014-04-29 11:56:33 +0200

    full refactor : split Controller in Dashboard & Mobile

---
+++ app/controllers/Dashboard.scala
@@ -0,0 +1,79 @@
+package controllers
+
+import play.api.mvc._
+import play.api.data.Form
+import play.api.data.Forms._
+import core.SpectatorManager
+
+
+/**
+ * Created with IntelliJ IDEA.
+ * Autor: julienderay
+ * Company : SERLI
+ * Date: 29/04/2014
+ * Time: 11:41
+ */
+
+object Dashboard extends Controller {
+
+  /**
+   * Forms
+   */
+  val loginForm = Form(
+    tuple(
+      "user" -> nonEmptyText,
+      "password" -> nonEmptyText
+    )
+  )
+
+  lazy val token = System.currentTimeMillis()
+
+  def dashboardLogin = Action { implicit request =>
+    request.session.get("token").fold(
+      Ok( views.html.dashboardLogin() )
+    ) (
+      token => Redirect( routes.Dashboard.dashboard() )
+    )
+  }
+
+  def dashboard = Action { implicit request =>
+    request.session.get("token").fold(
+      Redirect( routes.Dashboard.dashboardLogin() )
+    ) (
+      admin => {
+        if ( admin == token.toString )
+          Ok( views.html.dashboard( SpectatorManager.getSpectators ) )
+        else
+          Redirect( routes.Dashboard.dashboardLogin() )
+      }
+    )
+  }
+
+  def webSocket() = Action {
+    Ok( views.html.dashboardWebsocket() )
+  }
+
+  def interpretedJS() = Action {
+    Ok( views.html.interpretedJS() )
+  }
+
+  def authentification = Action { implicit request =>
+    loginForm.bindFromRequest.fold(
+      formWithErrors => {
+        Ok( views.html.dashboardLogin() )
+      },
+      user => {
+        if ( user._1 == "admin" && user._2 == "admin" )
+          Redirect( routes.Dashboard.dashboard() ).withSession( "token" -> token.toString )
+        else {
+          Ok( views.html.dashboardLogin() )
+        }
+      }
+    )
+  }
+
+  def dashboardLogout = Action {
+    Redirect( routes.Dashboard.dashboardLogin() ).withNewSession
+  }
+
+}
---
+++ app/controllers/Mobile.scala
@@ -0,0 +1,63 @@
+package controllers
+
+import play.api.mvc._
+import play.api.data.Form
+import play.api.data.Forms._
+import core.SpectatorManager
+
+object Mobile extends Controller {
+
+  val userName = Form(
+    "name" -> nonEmptyText
+  )
+
+  // --- Mobile ---
+
+  def mobile = Action { implicit request =>
+    request.session.get("user").fold(
+      Ok( views.html.mobileLogin() )
+    ) (
+      user => Redirect( routes.Mobile.mobileSpeak( user ) )
+    )
+  }
+
+  def mobileSpeak(name: String) = Action { implicit request =>
+    request.session.get("user").fold(
+      Redirect( routes.Mobile.mobile() )
+    ) (
+      user =>
+        if ( user == name )
+          Ok( views.html.mobileSpeak( name ) )
+        else
+          Redirect( routes.Mobile.mobile() )
+    )
+  }
+
+  def newUser = Action { implicit request =>
+    request.session.get("user").fold(
+      userName.bindFromRequest.fold(
+        formWithErrors => {
+          BadRequest( "Invalid form" )
+        },
+        userName => {
+          SpectatorManager.addSpectator( userName )
+          Redirect( routes.Mobile.mobileSpeak( userName ) ).withSession( "user" -> userName )
+        }
+      )
+    ) (
+      userName => Redirect( routes.Mobile.mobileSpeak( userName ) )
+    )
+  }
+
+  def logout = Action { implicit request => {
+      request.session.get("user").fold(
+        Redirect( routes.Mobile.mobile() )
+      ) (
+        userName => {
+          SpectatorManager.delSpectator( userName )
+          Redirect( routes.Mobile.mobile() ).withNewSession
+        }
+      )
+    }
+  }
+}
